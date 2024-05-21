0a9ebac4f7dcb2b9b1b699ef99ddd33ae9067b49
surajjha7428 <143312238+surajjha7428@users.noreply.github.com>
2024-05-21 20:41:46 +0530

    Done

---
+++ vite-project/src/components/Todo.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    if (input.trim()) {
+      setTodos([...todos, input]);
+      setInput("");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a todo"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
